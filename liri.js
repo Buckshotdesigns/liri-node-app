ce7a292a6801ea99abbeec3259669a631eb9e596
bryanbuckus <bryanbuckus@gmail.com>
2019-03-30 18:53:53 -0400

    adding to bands in town api

--- liri.js
+++ liri.js
@@ -1,8 +1,36 @@
 
-require("dotenv").config();
+// require("dotenv").config();
 
 var axios = require("axios");
 
-var keys = require("./keys.js");
+var keys = require("./key.js");
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = require('node-spotify-api');
+
+// var spotify = new Spotify(keys.spotify);
+
+var artist = "Motley Crue";
+
+axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+  function(response) {
+    console.log(JSON.stringify(result));
+  }
+);
+
+
+
+
+
+
+
+
+
+
+
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     }
+   
+//   console.log(data); 
+//   });
