ad9df192b7406b00332bdf4073ac303215604fa9
bryanbuckus <bryanbuckus@gmail.com>
2019-03-30 21:56:49 -0400

    laying out the rest of the functions

--- liri.js
+++ liri.js
@@ -3,20 +3,45 @@
 
 var axios = require("axios");
 
-var keys = require("./key.js");
+// var keys = require("./key.js");
+
+var moment = require("moment");
 
 // var spotify = require('node-spotify-api');
 
 // var spotify = new Spotify(keys.spotify);
 
-var artist = "slayer";
+var action = process.argv[2];
+var userInput = process.argv.slice(3).join(" ");
 
-axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-  function(response) {
-    console.log(response.data[0].venue.name);
-  }
-);
+    if (action === "concert-this") {
+        bandsInTown(userInput);
+    } if (action = "movie-this" ) {
+        movieOMDB(userInput);
+    } if (action === "spotify-this-song") {
+        spotifySong(userInput);
+    } if (action === "do-what-it-says"){
+        console.log("Go get me a large Farva");
+    };
 
+        function bandsInTown(userInput){
+            axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(
+                function(response) {
+                    // console.log(response.data);
+                    console.log(response.data[0].venue.name);
+                    console.log(response.data[0].venue.city);
+                    console.log(moment(response.data[0].datetime).format("MM-DD-YYYY"));
+                }
+            );
+        };
+
+        function movieOMDB(userInput) {
+
+        };
+
+        function spotifySong(userInput){
+            
+        }
 
 
 
